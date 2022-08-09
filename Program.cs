e6023621dc7163d30e080956b66bb669b213b9a2
Tatiana <ttt-ttt@bk.ru>
2022-08-09 05:39:28 +0300

    changed program - added methods

--- Program.cs
+++ Program.cs
@@ -3,3 +3,34 @@
 // можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется
 // пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+void FillArrayNew(string[] arrayOff, string[] arrayIn)
+{
+    int k = 0;
+    for (int i = 0; i < arrayOff.Length; i++)
+    {
+        if (arrayOff[i].Length < 4)
+        {
+            arrayIn[k] = arrayOff[i];
+            k++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int NumberOfElementsTreeOrLess(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
